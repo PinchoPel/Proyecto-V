703bfcf4ad7419a75b4b5244d0531fe02ee86339
PinchoPel <noxam13@gmail.com>
2024-09-14 11:14:15 +0200

    guardar

--- src/componentes/TresEnRaya/roundTresEnRaya.js
+++ src/componentes/TresEnRaya/roundTresEnRaya.js
@@ -9,33 +9,37 @@ import {victoriaTresEnRaya} from "./victoriaTresEnRaya";
 export let roundTresEnRaya = () =>{
 
   boardTresEnRaya();
-  
-      let jugador = 1;
 
-      gridSection.addEventListener("click", (event) =>{
-        
-        if (jugador > 2) {
-          jugador = 1;
-        };
+  let allButtons = document.querySelectorAll(".sectionButton");
+  let jugador = 1;
 
-        if (!event.target.classList.contains('jugador1') && !event.target.classList.contains('jugador2')) {
-          if (jugador == 1) {
-            let cross = document.createElement("img");
-            cross.id = "crossimg";
-            cross.src = "./cross-svgrepo-com.png";
-            event.target.classList = "jugador1";
-            jugador ++;
-            event.target.append(cross);
-          }
-          else if (jugador == 2) {
-            let circle = document.createElement("img");
-            circle.src = "./circle-svgrepo-com.png";
-            circle.id = "circleimg";
-            event.target.classList = "jugador2";
-            jugador ++;
-            event.target.append(circle);
-          }
-          victoriaTresEnRaya();
-       }
+  allButtons.forEach(element => {
+    
+    element.addEventListener("click", (event) =>{
+    
+      if (jugador > 2) {
+        jugador = 1;
+      };
+
+      if (!element.classList.contains('jugador1') && !element.classList.contains('jugador2')) {
+        if (jugador == 1) {
+          let cross = document.createElement("img");
+          cross.id = "crossimg";
+          cross.src = "./cross-svgrepo-com.png";
+          event.target.classList = "jugador1";
+          jugador ++;
+          event.target.append(cross);
+        }
+        else if (jugador == 2) {
+          let circle = document.createElement("img");
+          circle.src = "./circle-svgrepo-com.png";
+          circle.id = "circleimg";
+          event.target.classList = "jugador2";
+          jugador ++;
+          event.target.append(circle);
+        }
+        victoriaTresEnRaya();
+     }
 });
+  })
 }
